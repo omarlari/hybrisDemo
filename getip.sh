b17f4e7902cb1af688c605a0abb19b7e193c4e36
Omar Lari <omarlari@gmail.com>
2015-12-15 14:48:40 -0800

    fixing public ip on shell script

--- getip.sh
+++ getip.sh
@@ -2,6 +2,6 @@
 CFNSTACKID=$(cat ~/hybrisDemo/vars/env001.yml | grep stackPrefix | cut -d: -f2 | cut -c 2-)
 INSTANCEID=$(aws cloudformation list-stack-resources --stack-name $CFNSTACKID-serverCluster --region us-west-2 --query StackResourceSummaries[*].PhysicalResourceId --output text)
 INSTANCEIP=$(aws ec2 describe-instances --instance-id $INSTANCEID --region us-west-2 --query 'Reservations[].Instances[].NetworkInterfaces[].PrivateIpAddress' --output text)
-PUBLICIP=$(aws ec2 describe-instances --instance-id $INSTANCEID --region us-west-2 --query 'Reservations[].Instances[].NetworkInterfaces[].PublicIpAddress' --output text)
+PUBLICIP=$(aws ec2 describe-instances --instance-id $INSTANCEID --region us-west-2 --query 'Reservations[].Instances[].NetworkInterfaces[].Association[].PublicIp' --output text)
 echo IPADDRESS $INSTANCEIP
 echo PUBLICIP $PUBLICIP
