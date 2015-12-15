f9c3c750a2d737858f8ecf76ba8001947306b37d
Omar Lari <omarlari@gmail.com>
2015-12-15 09:05:34 -0800

    added getip script

---
+++ getip.sh
@@ -0,0 +1,5 @@
+# /bin/bash
+CFNSTACKID=$(cat ~/hybrisDemo/vars/env001.yml | grep stackPrefix | cut -d: -f2 | cut -c 2-)
+INSTANCEID=$(aws cloudformation list-stack-resources --stack-name $CFNSTACKID-serverCluster --region us-west-2 --query StackResourceSummaries[*].PhysicalResourceId --output text)
+INSTANCEIP=$(aws ec2 describe-instances --instance-id $INSTANCEID --region us-west-2 --query 'Reservations[].Instances[].NetworkInterfaces[].PrivateIpAddress' --output text)
+echo $INSTANCEIP
