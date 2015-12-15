bed0840c3d9799f49f062d3cb0a39db33b2dc80f
Omar Lari <omarlari@gmail.com>
2015-12-15 14:41:54 -0800

    added public IP functionality

--- goLive.sh
+++ goLive.sh
@@ -4,7 +4,7 @@
 AWS_EXT_DNS=$(aws route53 list-hosted-zones | grep -B 1 mckaws.omarlari.com | grep Id | awk -F\/ '{ print $3 }' | cut -d\" -f 1 | sed 2d)
 
 # Get the DNS Name of the Load Balancer
-AWS_INSTANCE_DNSNAME=$(curl -u reader:e8f8922efce5f8d861504a1131ce4f2a -s "10.99.254.83/job/hybrisGetIp/lastSuccessfulBuild/consoleText" | grep IPADDRESS | tail -n 1 | cut -d: -f2 | cut -c 11-)
+AWS_INSTANCE_DNSNAME=$(curl -u reader:e8f8922efce5f8d861504a1131ce4f2a -s "10.99.254.83/job/hybrisGetIp/lastSuccessfulBuild/consoleText" | grep PUBLICIP | tail -n 1 | cut -d: -f2 | cut -c 11-)
 
 # Create a temporary JSON file that is used to update DNS, leave the values that you are going to add blank
 
